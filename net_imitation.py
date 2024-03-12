bb1e7ae9b4ff8820ba31e8eb37f0e7c9b5395edc
ghost <i@nowtiloos.ru>
2024-03-12 22:33:46 +0300

    теория

--- net_imitation.py
+++ net_imitation.py
@@ -48,21 +48,3 @@ class Router:
         self.buffer.clear()
 
 
-router = Router()
-sv_from = Server()
-sv_from2 = Server()
-router.link(sv_from)
-router.link(sv_from2)
-router.link(Server())
-router.link(Server())
-sv_to = Server()
-router.link(sv_to)
-
-sv_from.send_data(Data("hello", sv_to.get_ip()))
-sv_from2.send_data(Data("hello", sv_to.get_ip()))
-sv_to.send_data(Data("hi", sv_from.get_ip()))
-
-router.send_data()
-msg_list_from = sv_from.get_data()
-msg_lst_to = sv_to.get_data()
-print(msg_lst_to, msg_list_from)
