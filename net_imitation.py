ea96d2857d8631c80f9e7cf7ce53f5326fb9caea
ghost <i@nowtiloos.ru>
2024-03-12 22:39:13 +0300

    ref

--- net_imitation.py
+++ net_imitation.py
@@ -1,50 +1,48 @@
 class Data:
-    def __init__(self, data, target_ip):
+    def __init__(self, data, target_ip) -> None:
         self.data = data
         self.target_ip = target_ip
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.data
 
 
 class Server:
     __ip_count = 1
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.router = None
         self.buffer = []
         self.ip = Server.__ip_count
         Server.__ip_count += 1
 
-    def get_ip(self):
+    def get_ip(self) -> int:
         return self.ip
 
-    def send_data(self, data: Data):
+    def send_data(self, data: Data) -> None:
         self.router.buffer.append(data)
 
-    def get_data(self):
+    def get_data(self) -> list:
         result = self.buffer[:]
         self.buffer.clear()
         return result
 
 
 class Router:
-    def __init__(self):
+    def __init__(self) -> None:
         self.net = {}
         self.buffer = []
 
-    def link(self, server: Server):
+    def link(self, server: Server) -> None:
         self.net.setdefault(server.ip, server.buffer)
         server.router = self
 
-    def unlink(self, server: Server):
+    def unlink(self, server: Server) -> None:
         server.router = None
         self.net.pop(server.ip)
 
-    def send_data(self):
+    def send_data(self) -> None:
         for data in self.buffer:
             self.net[data.target_ip].append(data)
 
         self.buffer.clear()
-
-
